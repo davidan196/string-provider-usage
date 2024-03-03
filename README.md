733325fa8491f69075deedb9153fc749c4cb9324
davidan196 <162036883+davidan196@users.noreply.github.com>
2024-03-03 22:26:44 +0700

    init

---
+++ README.md
@@ -0,0 +1,9 @@
+## Usage
+
+To install this package, you can run:
+
+`npm i string-provider-usage` 
+
+or
+
+`yarn add string-provider-usage` 
---
+++ index.js
@@ -0,0 +1,16 @@
+const {genString} = require('string-provider');
+const {utcDateTimeFormat} = require('moment-datetime-js');
+
+function genString(length) {
+    return genString(length)
+}
+
+function utcDateTimeFormat() {
+    return utcDateTimeFormat();
+}
+
+module.exports = {
+    genString,
+    utcDateTimeFormat
+}
+
